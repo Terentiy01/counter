f944fc61a35ae9dc58824128f0bb5fd754a59a83
Almaz <almazius11@gmail.com>
2023-02-06 20:39:32 +0300

    created project structure and layout

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './index.scss'
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <h2>Счетчик:</h2>
+        <h1>0</h1>
+        <button className="minus">- Минус</button>
+        <button className="plus">Плюс +</button>
+      </div>
+    </div>
+  )
+}
+
+export default App
